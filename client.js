64ed5a57ce653304943d8d50632253e691acc9eb
Alex Li <lialex0218@gmail.com>
2021-06-03 02:58:23 +0000

    snake-1st

---
+++ client.js
@@ -0,0 +1,22 @@
+const { builtinModules } = require("module");
+const net = require("net");
+
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "135.23.222.131", // IP address here,
+    port: 50542 // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("data", () => {
+    // code that does something when the
+    console.log('you ded cuz you idled');
+  });
+  
+  return conn;
+};
+
+module.exports = connect;
