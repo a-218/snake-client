5ed206fb6ef402db96529691877430210cc52d29
Alex Li <lialex0218@gmail.com>
2021-06-03 03:45:58 +0000

    handle user input

--- play.js
+++ play.js
@@ -6,5 +6,27 @@
 const net = require("net");
 const connect = require('./client');
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  // your code here
+  process.stdin.on('data', (key) => {
+    process.stdout.write('.');
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+};
+
+
+
+setupInput();
 console.log("Connecting ...");
 connect();
